d0657d57a60f8df0c57da991ca4a6e110e790be1
rverma-dev <rverma-dev@gmail.com>
2024-03-27 18:13:45 +0530

    flink

--- apps/openmeter/event.py
+++ apps/openmeter/event.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import time
 import uuid
 import json
 import random
@@ -6,18 +6,23 @@ import random
 # Generate a random CloudEvent in the specified format
 def generate_random_cloudevent():
     event = {
-        "specversion": "1.0",
-        "type": "com.example.type",
-        "source": f"/example/source/{random.randint(1, 100)}",
         "id": str(uuid.uuid4()),
-        "time": datetime.now().isoformat(),
+        "specversion": "1.0",
+        "type": "request",
+        "source": f"/example/source/{random.randint(1, 100)}",
+        "subject": "customer-2",
+        "time": int(round(time.time() * 1000)),
         "datacontenttype": "application/json",
-        "data": {
-            "value": random.randint(1, 100)
-        },
+        "data": json.dumps({
+            "method": "POST",
+            "path": "/",
+            "region": "us-east-1",
+            "zone": "a",
+            "duration_ms": random.randint(1, 100)
+        }),
     }
     return json.dumps(event)
 
-for x in range(0, 100):
+for x in range(0, 600):
     random_cloudevent_json = generate_random_cloudevent()
     print(random_cloudevent_json)
